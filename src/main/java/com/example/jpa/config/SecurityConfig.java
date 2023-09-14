ec08f961dce424d317fcaa2f09e980a4fcab940a
Jennny1 <june9282@gmail.com>
2023-09-14 23:01:38 +0900

    "41번"

--- src/main/java/com/example/jpa/config/SecurityConfig.java
+++ src/main/java/com/example/jpa/config/SecurityConfig.java
@@ -2,14 +2,28 @@ package com.example.jpa.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfig {
-
   @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+        .httpBasic().disable()
+        .csrf().disable()
+        .cors().and()
+        .authorizeRequests()
+        .antMatchers("/api/**").permitAll()
+        .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll() // join, login은 언제나 가능
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // STATELESS = jwt사용하는 경우 씀 : 매번 토큰을 사용하는 개념?
+        .and()
+//                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class) //UserNamePasswordAuthenticationFilter적용하기 전에 JWTTokenFilter를 적용 하라는 뜻 입니다.
+        .build();
   }
 }
--- src/main/java/com/example/jpa/user/controller/ApiUserController.java
+++ src/main/java/com/example/jpa/user/controller/ApiUserController.java
@@ -1,7 +1,5 @@
 package com.example.jpa.user.controller;
 
-import static com.example.jpa.Sample1Application.main;
-
 import com.example.jpa.notice.entity.Notice;
 import com.example.jpa.notice.model.NoticeResponse;
 import com.example.jpa.notice.model.ResponseError;
@@ -13,7 +11,6 @@ import com.example.jpa.user.exception.PasswordNoMatchException;
 import com.example.jpa.user.exception.UserNotFoundException;
 import com.example.jpa.user.model.UserInput;
 import com.example.jpa.user.model.UserInputFind;
-import com.example.jpa.user.model.UserInputPassword;
 import com.example.jpa.user.model.UserResponse;
 import com.example.jpa.user.model.UserUpdate;
 import java.time.LocalDateTime;
@@ -31,7 +28,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -41,10 +37,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 public class ApiUserController {
-    // 사용자 입력값: 이메일(ID), 이름, 비밀번호, 연락처
+  // 사용자 입력값: 이메일(ID), 이름, 비밀번호, 연락처
 
-    private final UserRepository userRepository;
-    private final NoticeRepository noticeRepository;
+  private final UserRepository userRepository;
+  private final NoticeRepository noticeRepository;
 /*
 유저 정보 등록
  */
@@ -102,76 +98,74 @@ public class ApiUserController {
     }
 
 */
+/*
+  *//*
+  사용자등록 (이메일 예외처리)
+   *//*
+  @PostMapping("/api/user")
+  public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
 
-    /*
-    사용자등록 (이메일 예외처리)
-     */
-    @PostMapping("/api/user")
-    public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
-
-        List<ResponseError> responseErrorList = new ArrayList<>();
-        if (errors.hasErrors()) {
-            errors.getAllErrors().stream().forEach((e) -> {
-                responseErrorList.add(ResponseError.of((FieldError)e));
-            });
-                return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
-        }
-
-
-        if (userRepository.countByEmail(userInput.getEmail()) > 0) {
-          throw new ExistsEmailExeption("이미 존재하는 이메일");
-        }
-
-
-        User user = User.builder()
-            .email(userInput.getEmail())
-            .userName(userInput.getUserName())
-            .password(userInput.getPassword())
-            .phone((userInput.getPhone()))
-            .regDate(LocalDateTime.now())
-            .build();
-        userRepository.save(user);
-        return ResponseEntity.ok().build();
+    List<ResponseError> responseErrorList = new ArrayList<>();
+    if (errors.hasErrors()) {
+      errors.getAllErrors().stream().forEach((e) -> {
+        responseErrorList.add(ResponseError.of((FieldError) e));
+      });
+      return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
     }
 
-    // ExceptionHandler
-    @ExceptionHandler(value = {UserNotFoundException.class, ExistsEmailExeption.class,
-        PasswordNoMatchException.class})
-    public ResponseEntity<?> ExistsEmailExeptionHandler(RuntimeException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
-
+    if (userRepository.countByEmail(userInput.getEmail()) > 0) {
+      throw new ExistsEmailExeption("이미 존재하는 이메일");
     }
+
+    User user = User.builder()
+        .email(userInput.getEmail())
+        .userName(userInput.getUserName())
+        .password(userInput.getPassword())
+        .phone((userInput.getPhone()))
+        .regDate(LocalDateTime.now())
+        .build();
+    userRepository.save(user);
+    return ResponseEntity.ok().build();
+  }
+
+  // ExceptionHandler
+  @ExceptionHandler(value = {UserNotFoundException.class, ExistsEmailExeption.class,
+      PasswordNoMatchException.class})
+  public ResponseEntity<?> ExistsEmailExeptionHandler(RuntimeException exception) {
+    return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+
+  }*/
 
 
     /*
     유저정보 수정
      */
 
-    @PutMapping("/api/user/{id}")
-    public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody UserUpdate userUpdate,
-        Errors errors) {
+  @PutMapping("/api/user/{id}")
+  public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody UserUpdate userUpdate,
+      Errors errors) {
 
-        // 에러처리
-        List<ResponseError> responseErrorList = new ArrayList<>();
-        if (errors.hasErrors()) {
-            // 에러가 있을 때 처리
-            errors.getAllErrors().forEach((e) -> {
-                responseErrorList.add(ResponseError.of((FieldError) e));
+    // 에러처리
+    List<ResponseError> responseErrorList = new ArrayList<>();
+    if (errors.hasErrors()) {
+      // 에러가 있을 때 처리
+      errors.getAllErrors().forEach((e) -> {
+        responseErrorList.add(ResponseError.of((FieldError) e));
 
-            });
-            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
-        }
-
-        // 사용자 정보가 없을 때 UserNotFountException 발생// "사용자 정보가 없습니다."
-        User user = userRepository.findById(id)
-            .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
-
-        user.setPhone(userUpdate.getPhone());
-        user.setUpdateDate(LocalDateTime.now());
-        userRepository.save(user);
-
-        return ResponseEntity.ok().build();
+      });
+      return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
     }
+
+    // 사용자 정보가 없을 때 UserNotFountException 발생// "사용자 정보가 없습니다."
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
+
+    user.setPhone(userUpdate.getPhone());
+    user.setUpdateDate(LocalDateTime.now());
+    userRepository.save(user);
+
+    return ResponseEntity.ok().build();
+  }
 
 
     /*
@@ -179,38 +173,38 @@ public class ApiUserController {
     보안상 비밀번호, 가입일, 수정일은 x
      */
 
-    @GetMapping("/api/user/{id}")
-    public UserResponse getUser(@PathVariable Long id) {
-        User user = userRepository
-            .findById(id)
-            .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
+  @GetMapping("/api/user/{id}")
+  public UserResponse getUser(@PathVariable Long id) {
+    User user = userRepository
+        .findById(id)
+        .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
 
-        // UserResponse userResponse = new UserResponse(user);
-        UserResponse userResponse = UserResponse.of(user);
+    // UserResponse userResponse = new UserResponse(user);
+    UserResponse userResponse = UserResponse.of(user);
 
-        return userResponse;
+    return userResponse;
 
-    }
+  }
 
-    /*
-    35. 공지사항 목록
-    삭제일, 삭제자 아이디는 보안상 보여주지 않음
-    작성자의 아이디와 이름만 보여줌
-     */
-    @GetMapping("/api/user/{id}/notice")
-    public List<NoticeResponse> userNotice(@PathVariable Long id) {
-        User user = userRepository.findById(id)
-            .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
+  /*
+  35. 공지사항 목록
+  삭제일, 삭제자 아이디는 보안상 보여주지 않음
+  작성자의 아이디와 이름만 보여줌
+   */
+  @GetMapping("/api/user/{id}/notice")
+  public List<NoticeResponse> userNotice(@PathVariable Long id) {
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
 
-        List<Notice> noticeList = noticeRepository.findByUser(user);
-        List<NoticeResponse> noticeResponseList = new ArrayList<>();
+    List<Notice> noticeList = noticeRepository.findByUser(user);
+    List<NoticeResponse> noticeResponseList = new ArrayList<>();
 
-        noticeList.stream().forEach((e) -> {
-            noticeResponseList.add(NoticeResponse.of(e));
-        });
+    noticeList.stream().forEach((e) -> {
+      noticeResponseList.add(NoticeResponse.of(e));
+    });
 
-        return noticeResponseList;
-    }
+    return noticeResponseList;
+  }
 
 
     /*
@@ -220,124 +214,123 @@ public class ApiUserController {
     "비밀번호가 일치하지 않습니다."
      */
 
-    @PatchMapping("/api/user/{id}/pasword")
-    public ResponseEntity<?> updateUserPassword(@PathVariable long id,
-        @RequestBody UserInputPassword userInputPassword, Errors errors) {
+/*  @PatchMapping("/api/user/{id}/pasword")
+  public ResponseEntity<?> updateUserPassword(@PathVariable long id,
+      @RequestBody UserInputPassword userInputPassword, Errors errors) {
 
-        // 에러처리
-        List<ResponseError> responseErrorList = new ArrayList<>();
-        if (errors.hasErrors()) {
-            // 에러가 있을 때 처리
-            errors.getAllErrors().forEach((e) -> {
-                responseErrorList.add(ResponseError.of((FieldError) e));
+    // 에러처리
+    List<ResponseError> responseErrorList = new ArrayList<>();
+    if (errors.hasErrors()) {
+      // 에러가 있을 때 처리
+      errors.getAllErrors().forEach((e) -> {
+        responseErrorList.add(ResponseError.of((FieldError) e));
 
-            });
-            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
-        }
+      });
+      return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
+    }
 
-        User user = userRepository.findByIdAndPassword(id, userInputPassword.getPassword())
-            .orElseThrow(() -> new PasswordNoMatchException("비밀번호가 일치하지 않습니다."));
+    User user = userRepository.findByIdAndPassword(id, userInputPassword.getPassword())
+        .orElseThrow(() -> new PasswordNoMatchException("비밀번호가 일치하지 않습니다."));
 
-        // 기존 비밀번호가 일치하였을 때
-        user.setPassword(userInputPassword.getNewPassword());
-        userRepository.save(user);
+    // 기존 비밀번호가 일치하였을 때
+    user.setPassword(userInputPassword.getNewPassword());
+    userRepository.save(user);
 
-        return ResponseEntity.ok().build();
+    return ResponseEntity.ok().build();
+  }*/
+
+  // 패스워드 암호화
+  private String getEncryptPassword(String password) {
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    return bCryptPasswordEncoder.encode(password);
+  }
+
+  /*
+  회원가입 시 비밀번호를 암호화하여 저장
+   */
+  @PostMapping("/api/user")
+  public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
+
+    List<ResponseError> responseErrorList = new ArrayList<>();
+    if (errors.hasErrors()) {
+      errors.getAllErrors().stream().forEach((e) -> {
+        responseErrorList.add(ResponseError.of((FieldError) e));
+      });
+      return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
+    }
+
+    if (userRepository.countByEmail(userInput.getEmail()) > 0) {
+      throw new ExistsEmailExeption("이미 존재하는 이메일");
     }
 
     // 패스워드 암호화
-    private String getEncryptPassword(String password) {
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        return bCryptPasswordEncoder.encode(password);
-    }
+    String encryptPassword = getEncryptPassword(userInput.getPassword());
+    System.out.println("암호 : " + encryptPassword);
 
-    /*
-    회원가입 시 비밀번호를 암호화하여 저장
-     *//*
-    @PostMapping("/api/user")
-    public ResponseEntity<?> addUser(@RequestBody @Valid UserInput userInput, Errors errors) {
+    User user = User.builder()
+        .email(userInput.getEmail())
+        .userName(userInput.getUserName())
+        .password(encryptPassword)
+        .phone((userInput.getPhone()))
+        .regDate(LocalDateTime.now())
+        .build();
+    userRepository.save(user);
+    return ResponseEntity.ok().build();
 
-        List<ResponseError> responseErrorList = new ArrayList<>();
-        if (errors.hasErrors()) {
-            errors.getAllErrors().stream().forEach((e) -> {
-                responseErrorList.add(ResponseError.of((FieldError) e));
-            });
-            return new ResponseEntity<>(responseErrorList, HttpStatus.BAD_REQUEST);
-        }
+  }
 
-        if (userRepository.countByEmail(userInput.getEmail()) > 0) {
-            throw new ExistsEmailExeption("이미 존재하는 이메일");
-        }
+  /*
+  회원탈퇴
+  회원정보가 존재하지 않을 때는 예외처리
+  사용자가 등록한 게시글이 있는 경우에는 회원 삭제가 되지 않음
+   */
+  @DeleteMapping("/api/user/{id}")
+  public ResponseEntity<?> deleteUser(@PathVariable long id) {
 
-        // 패스워드 암호화
-        String encryptPassword = getEncryptPassword(userInput.getPassword());
-        System.out.println("암호 : " + encryptPassword);
+    // 사용자 정보가 없을 때 UserNotFountException 발생// "사용자 정보가 없습니다."
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
 
-        Uuser user = Uuser.builder()
-            .email(userInput.getEmail())
-            .userName(userInput.getUserName())
-            .password(encryptPassword)
-            .phone((userInput.getPhone()))
-            .regDate(LocalDateTime.now())
-            .build();
-        userRepository.save(user);
-        return ResponseEntity.ok().build();
-
-    }
-    */
-    /*
-    회원탈퇴
-    회원정보가 존재하지 않을 때는 예외처리
-    사용자가 등록한 게시글이 있는 경우에는 회원 삭제가 되지 않음
-     */
-    @DeleteMapping("/api/user/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable long id) {
-
-        // 사용자 정보가 없을 때 UserNotFountException 발생// "사용자 정보가 없습니다."
-        User user = userRepository.findById(id)
-            .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
-
-        
-        // 등록한 게시글이 있는지 확인
+    // 등록한 게시글이 있는지 확인
 
         /*
         39. 탈퇴(유저삭제)
         회원정보가 존재하지 않는 경우 예외처리
         만약 회원이 게시판에 글을 쓴 경우에는 회원삭제가 되지 않음
          */
-        
-        try {
-            userRepository.delete(user);
-        } catch (DataIntegrityViolationException e) {
-            String message = "제약조건에 문제가 발생하였습니다.";
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
 
-        }catch (Exception e) {
-            String message = "회원 탈퇴 중 문제가 발생하였습니다.";
-            return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-        }
+    try {
+      userRepository.delete(user);
+    } catch (DataIntegrityViolationException e) {
+      String message = "제약조건에 문제가 발생하였습니다.";
+      return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
 
-        return ResponseEntity.ok().build();
-
+    } catch (Exception e) {
+      String message = "회원 탈퇴 중 문제가 발생하였습니다.";
+      return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 
-    /*
-    40. 사용자 이메일 검색, 찾기
-    이름과 전화번호에 해당하는 이메일을 찾는다.
-     */
-    @GetMapping("/api/user/")
-    public ResponseEntity<?> findUser(@RequestBody UserInputFind userInputFind) {
-        
-        // 유저 이름과 폰으로 검색
-        User user = userRepository.findByUserNameAndPhone(userInputFind.getUserName(), userInputFind.getPhone())
-            .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
+    return ResponseEntity.ok().build();
 
+  }
 
-        // 검색 결과가 있을 때, 정보 리턴
-        UserResponse userResponse = UserResponse.of(user);
+  /*
+  40. 사용자 이메일 검색, 찾기
+  이름과 전화번호에 해당하는 이메일을 찾는다.
+   */
+  @GetMapping("/api/user/")
+  public ResponseEntity<?> findUser(@RequestBody UserInputFind userInputFind) {
 
-        return ResponseEntity.ok().body(userResponse);
-    }
+    // 유저 이름과 폰으로 검색
+    User user = userRepository.findByUserNameAndPhone(userInputFind.getUserName(),
+            userInputFind.getPhone())
+        .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
+
+    // 검색 결과가 있을 때, 정보 리턴
+    UserResponse userResponse = UserResponse.of(user);
+
+    return ResponseEntity.ok().body(userResponse);
+  }
 
 
     /*
@@ -349,25 +342,41 @@ public class ApiUserController {
      */
 
 
-    private String getResetPassword(){
-        return UUID.randomUUID().toString().replaceAll("-", "").substring(0,10);
+  private String getResetPassword() {
+    return UUID.randomUUID().toString().replaceAll("-", "").substring(0, 10);
 
-    }
+  }
 
-    @GetMapping("/api/user/{id}/password/reset")
-    public void resetUserPassword(@PathVariable Long id) {
+  @GetMapping("/api/user/{id}/password/reset")
+  public ResponseEntity<?> resetUserPassword(@PathVariable Long id) {
 
-        // 유저 아이디로 검색
-        User user = userRepository.findById(id)
-            .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
+    // 유저 아이디로 검색
+    User user = userRepository.findById(id)
+        .orElseThrow(() -> new UserNotFoundException("사용자 정보가 없습니다."));
 
-        // 비밀번호 초기화
-        String resetEncryptPassword = getEncryptPassword(getResetPassword());
-        user.setPassword(resetEncryptPassword);
-        userRepository.save(user);
+    // 비밀번호 초기화
+    String resetPassword = getResetPassword();
+    String resetEncryptPassword = getEncryptPassword(resetPassword);
+    user.setPassword(resetEncryptPassword);
+    userRepository.save(user);
+
+    String message = String.format("[%s]님의 임시비밀번호가 [%s]로 초기화 되었습니다."
+        , user.getUserName()
+        , resetPassword);
+
+    sensSMS(message);
+
+    return ResponseEntity.ok().build();
+
+  }
+
+  void sensSMS(String message) {
+    System.out.println("[문자메시지 전송]");
+    System.out.println(message);
+
+  }
 
 
-        }
 
 
 }
